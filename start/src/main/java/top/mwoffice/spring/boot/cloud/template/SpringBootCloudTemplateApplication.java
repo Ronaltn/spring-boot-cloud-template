598addb27263ae01523816f59dc502261bdc1a49
Martin Wang <ronaltn@live.cn>
2021-03-11 16:08:10 +0800

    用https://start.aliyun.com创建

---
+++ start/src/main/java/top/mwoffice/spring/boot/cloud/template/SpringBootCloudTemplateApplication.java
@@ -0,0 +1,13 @@
+package top.mwoffice.spring.boot.cloud.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCloudTemplateApplication.class, args);
+    }
+
+}
---
+++ start/src/test/java/top/mwoffice/spring/boot/cloud/template/SpringBootCloudTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mwoffice.spring.boot.cloud.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
